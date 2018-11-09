7a63c4b39c58b0ac4393e90a2325345cbd98b8e1
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-11-09 17:04:56 +0100

    footer responsive

---
+++ src/components/footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import footer from './index.css';
+
+class Footer extends React.Component{
+    render() {
+      return (
+        <div>
+            <footer>
+                <ul class="footer-list">
+                    <li><a className="footer-link" href="">Legal</a></li>
+                    <li><a className="footer-link" href="">About us</a></li>
+                    <li><a className="foote-link">To Brussels ©</a></li>
+                </ul>
+            </footer>      
+        </div>
+      );
+    }
+  }
+  
+export default Footer;
