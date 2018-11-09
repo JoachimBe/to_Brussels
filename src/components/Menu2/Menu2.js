4bd52e25a587bd79533c77a6a5921e3d77ab14a2
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-11-09 09:37:40 +0100

    initialisation de menu2

---
+++ src/components/Menu2/Menu2.js
@@ -0,0 +1,14 @@
+import React from'react';
+import './main.css';
+
+class Menu2 extends React.Component{
+    constructor(){
+        super.props();
+    }
+
+    render()
+        return(
+        )
+}
+
+export default Menu2;
