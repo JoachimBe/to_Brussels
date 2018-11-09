b3b65428d5b8b04704356eef98eeb24fa0e0dd4a
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-11-09 10:21:57 +0100

    creation de formulaire

--- src/components/Menu2/Menu2.js
+++ src/components/Menu2/Menu2.js
@@ -1,11 +1,21 @@
 import React from'react';
 import './main.css';
 
-class Menu2 {
+class Menu2 extends React.Component{
+    constructor(){
+        super()
+    }
     render(){
         return(
             <div>
                 <form>
+                    <p>Course</p>
+                    <ol>
+                    <li><input type="radio" name="kilometers" value="5"></input>5 Km</li>
+                    <li><input type="radio" name="kilometers" value="10"></input>10 Km</li>
+                    <li><input type="radio" name="kilometers" value="15"></input>15 Km</li>
+                    <li><input type="radio" name="kilometers" value="20"></input>20 Km</li>
+                    </ol>
                 </form>
             </div>
         );
