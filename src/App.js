0bddffd156cbd6166ce92b17378ff58e7872eeca
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-11-14 10:54:20 +0100

    optimisation + correction / modif mediaq

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
-import  './components/Header/main.css';
+
 
 class App extends Component {
   render() {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './main.css'
 import save from './save2.png';
 import print from './printt.png';
 
@@ -7,9 +8,9 @@ class Header extends React.Component{
         return( <div className="header">
             
             <h1 className="title">To Brussels</h1>
-            <div className="btn_group">
-            <button className="button-right"><img src={save} className="logo-right" /></button>
-            <button className="button-right"><img src={print} className="logo-right" /></button>
+            <div className="header-buttons">
+            <button className="header-button"><img src={save} className="icon" /></button>
+            <button className="header-button"><img src={print} className="icon" /></button>
             </div>
         </div>
         )
