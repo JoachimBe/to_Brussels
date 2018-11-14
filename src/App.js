82231074acb9b75d35e5982df19d37f110be6512
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-11-14 09:33:30 +0100

    ned

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Menu2 from './components/Menu2/Menu2.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
       <Menu2/>
-      </div>
     );
   }
 }
