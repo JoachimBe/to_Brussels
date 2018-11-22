57ba18b110355d0ae0b4310a191a81901a282bae
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-11-22 16:29:05 +0100

    OUIIII, la localisation fonctionne! :OOO

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,22 +5,25 @@ import GoogleMapReact from 'google-map-react';
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
+  state= {
+    location:{
       lat: 50.8504500,
       lng: 4.3487800
     },
-    zoom: 11
-  };
+      zoom: 12,
+      haveUsersLocation: false,
+    }
 
+  
   render() {
+    const position = [this.state.location.lat, this.state.location.lng];
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyCPzxx1Hx18ZT4q2ONjkyFWYRVhlmNrN-I' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          center={position}
+          defaultZoom={this.state.zoom}
         >
           <AnyReactComponent
             lat={50.8504500}
@@ -31,6 +34,36 @@ class SimpleMap extends Component {
         </div>
     );
   }
+  getLocation = ()=> {
+    return new Promise((resolve) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        resolve({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      }, () => {      
+        resolve(fetch('https://ipapi.co/json')
+          .then(res => res.json())
+          .then(location => {
+            return {
+              lat: location.latitude,
+              lng: location.longitude
+            };
+          }));
+      });
+    });
+  }
+  componentDidMount(){
+    this.getLocation()
+      .then(location =>{
+        this.setState({
+          location,
+          haveUserLocation: true,
+          zoom:16
+        })
+      })
+  }
+
 }
 
 export default SimpleMap;
