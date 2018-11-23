abecf02f791daaaed1b07eb51d7da7debc8765c2
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-11-23 14:04:50 +0100

    en cours param navigation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DataProvider } from './ContextAPI/Context';
+import Test from './components/Test/index';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      places : {}
+    }
+  }
+
+  handlePlacesChoice(data){
+    console.log('data', data)
+    this.setState({
+      places : data
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <DataProvider value={this.state.places} >
+          <Test callback={(data) => this.handlePlacesChoice(data)} />
+        </DataProvider>
       </div>
     );
   }
 }
 
 export default App;
+ 
---
+++ src/ContextAPI/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function App() {
+  return <Selection />;
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
