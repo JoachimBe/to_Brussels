fdfc465519016bfe1f50bd15a0b79f97dd2f72f5
Anja Razafitrimo <anjazarafitrimo@msn.com>
2018-11-25 15:34:10 +0100

    commit déjà fait pourtant demande nouveau commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
-import './App.css';
-import Menu2 from './components/Menu2/Menu2.js'
-import Legal from './components/legal/index.js'
-import Footer from './components/footer/index.js'
-
+import SideMenu from "./components/Sidemenu/Sidemenu.js";
+import Backdrop from "./components/backdrop/backdrop.js";
 class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      sideMenuOpen: false,
+      places : {}
+    };
+  }
+
+  handleChange(data){
+console.log(data);
+    this.setState({
+      places:data
+    })
+  }
+
+  menuToggleClickHandler = () =>{
+    this.setState((prevState)=>{
+      return {sideMenuOpen:!prevState.sideMenuOpen};
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({sideMenuOpen:false})
+  };
+
+
   render() {
+    
+    let backdrop;
+
+    if(this.state.sideMenuOpen){      
+      backdrop= <Backdrop click={this.backdropClickHandler}/>;
+    }
     return (
-      
-      <div className="App">
-        <Menu2/>
-        <Legal/>
-        <Footer/>
+      <div>
+        <SideMenu show={this.state.sideMenuOpen} menuClickHandler={this.menuToggleClickHandler} />
       </div>
     );
   }
