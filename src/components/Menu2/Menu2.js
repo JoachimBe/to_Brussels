8c876e1357c40b37dfd4d8b537de758fa9bd78d2
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-11-28 17:18:56 +0100

    envoi des champs dans le state

--- src/components/Menu2/Menu2.js
+++ src/components/Menu2/Menu2.js
@@ -1,5 +1,7 @@
 import React from'react';
 import './main.css';
+import MenuToggleButton from "../ToggleButton/MenuToggleButton.js";
+
 import ReactDOM from 'react-dom';
 
 
@@ -10,30 +12,55 @@ class Menu2 extends React.Component{
         super(props);
         this.handleShowClick = this.handleShowClick.bind(this);
         this.handleHideClick = this.handleHideClick.bind(this);
-        this.state = {visible : true};
+        this.state = {
+            visible : true,
+            kilometres: 0,
+            destination: {
+                adress: ''
+            }
+        };
+        this.onChange = this.onChange.bind(this);
+        this.submitForm = this.submitForm.bind(this);
     }
+
     render(){
         const visible = this.state.visible;
         
         return(
-            <div className="sideBar">
-                <form action="" method="">
-                <input type="radio" name="course" value="course_simple"></input><span className="typeParcour" onClick={ev => this.showKilom()}> Course</span>
-                    <ol name="listK" className={this.state.display}>
-                        <li><input type="radio" name="kilometers" value="5"></input><label for='5'>5 Km</label></li>
-                        <li><input type="radio" name="kilometers" value="10"></input><label for='10'>10 Km</label></li>
-                        <li><input type="radio" name="kilometers" value="20"></input><label for='15'>15 Km</label></li>
-                        <li><input type="radio" name="kilometers" value="15"></input><label for='20'>20 Km</label></li>
-                    </ol>
-                    <input type="radio" name="loop" value='loop' className={this.state.display}></input><label className={this.state.display}>In Loop</label>
-                    <label className="typeParcour lab4input" for='adress'>To an adress:</label>
-                    <input type="text" name="adress" required size="3" onClick={ev => this.hideKiloM()}/>
+            <div  className={this.props.show ? "side-menu.open" : "sideBar"}>
+                    <MenuToggleButton click={this.props.menuClickHandler}/>
+
+                        <img className="logo-dessus" src="https://i.goopics.net/2GKx8.png" alt="logo"></img>
+
+            <div className="contenu">
+            <p className="welcome">To Brussels<p className="welcome-text">You can either choose a fixed distance that will take you over a loop course, or enter a destination adress, if you already know where to go ;-)</p> </p>
+                <form className="form" action="" method="" onSubmit={this.submitFrom}>
+                <p className="typeParcour" onClick={ev => this.showKilom()}>How big is your will to visit ?</p>
+                    <ul name="listK" className={this.state.display}>
+                        <li><input className="choix-km" type="radio" name="kilometers" value="5" onInput={ev => this.setKilometers(5)}></input><label className="choix-km" htmlFor='5'>5 Km</label></li>
+                        <li><input className="choix-km" type="radio" name="kilometers" value="10" onInput={ev => this.setKilometers(10)}></input><label className="choix-km" htmlFor='10'>10 Km</label></li>
+                        <li><input className="choix-km" type="radio" name="kilometers" value="20" onInput={ev => this.setKilometers(15)}></input><label className="choix-km" htmlFor='15'>15 Km</label></li>
+                        <li><input className="choix-km" type="radio" name="kilometers" value="15" onInput={ev => this.setKilometers(20)}></input><label className="choix-km" htmlFor='20'>20 Km</label></li>
+                    </ul>
+                    <label className="typeParcour lab4input" for='adress'>Your final destination:</label>
+                    <input className="adress" type="text" name="adress" required size="22" value={this.state.adress} onChange={this.handleChangekilometers.bind(this)} onClick={ev => this.hideKiloM()}/>
                     
                     <input type="submit" className="buttonTwo" value="Go to map"/>
-                </form>
-                <button type="button"> Retour</button>
+                </form> 
+                <a className="back" href="#"><img className="back" src="https://i.goopics.net/NNmvW.png" alt="back"></img></a>
+                </div>
             </div>
         );
+    }
+
+    handleChangekilometers(event){
+        this.setState({
+            kilometers: event.target.value
+        });
+    }
+
+    setAdress(e){
+        this.setState({value:e})
     }
 
     hideKiloM(){
@@ -45,7 +72,7 @@ class Menu2 extends React.Component{
     showKilom(){
         let nameToShow = document.getElementsByName('listK');
         for(let k = 0; k < nameToShow.length; k++){
-            this.setState({display : "show"});
+            this.setState({display : ""});
         }
     }
 
@@ -54,6 +81,14 @@ class Menu2 extends React.Component{
     }
     handleHideClick(){
         this.setState({visible : false});
+    }
+    onChange (e){
+        this.setState({
+            /////////////////////////////////////////
+        });
+    }
+    submitForm(e){
+        e.preventDefault();
     }
 }
 
