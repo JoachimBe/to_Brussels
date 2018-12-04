417228ce8206198b7a189bc1256e667afcf3b24d
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-12-04 10:52:10 +0100

    modif avant lien map

--- src/components/API/index.js
+++ src/components/API/index.js
@@ -8,13 +8,16 @@ class DisplayApi extends Component {
     super(props);
     this.state = {
       places: {
-      
+      museum:true,
+      monuments:true
       },
       location: [],
+      mapLocation: [],
       user: {
         "lat": 50.8503,
         "lng": 4.3517
-      }
+      },
+      isReady: false
     }
   };
 
@@ -23,6 +26,14 @@ class DisplayApi extends Component {
       places: event.target.value,
     });
   }
+
+  getLocation(){
+    let direction = this.state.location.map((elem, index) => {
+      return elem.geometry.location
+    }) 
+    this.setState({mapLocation: direction})
+  }
+
 
   callPlaceApi(type) {
     fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${type}&location=${this.state.user.lat},${this.state.user.lng}&radius=${this.state.userdistance}&key=AIzaSyCPzxx1Hx18ZT4q2ONjkyFWYRVhlmNrN-I`
@@ -33,9 +44,10 @@ class DisplayApi extends Component {
       })
       .then(response => response.json())
       .then(data => {
-        this.setState({
-          location: data.results
-        });
+
+        this.setState(prevState => ({
+          location: [...prevState.location, ...data.results]
+        }));
       });
   }
 
@@ -45,6 +57,15 @@ class DisplayApi extends Component {
         this.callPlaceApi(locationName[count])
       }
       count++
+    }
+    this.setState({
+      isReady : true
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.location !== this.state.location){
+      this.getLocation()
     }
   }
 
