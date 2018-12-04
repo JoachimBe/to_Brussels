bb0bc5b8bb61f0e27e91aca892c0a1f68768abb4
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-12-04 09:55:38 +0100

    en cours

--- src/components/Parcours/index.js
+++ src/components/Parcours/index.js
@@ -1,20 +1,40 @@
+import React, {Component} from 'react';
 
-var parcours = [
-	new google.maps.LatLng(46.781367900048, 6.6401992834884),
-	new google.maps.LatLng(46.780821285011, 6.6416348016222),
-	new google.maps.LatLng(46.780496546047, 6.6421830461926),
-	new google.maps.LatLng(46.779835306991, 6.6426765713417),
-	new google.maps.LatLng(46.777748677169, 6.6518819126808),
-	new google.maps.LatLng(46.778027878803, 6.6541349682533),
-	new google.maps.LatLng(46.778484884759, 6.6557324922045),
-	new google.maps.LatLng(46.778752327087, 6.6573654211838),
-	new google.maps.LatLng(46.778605381016, 6.6588674582321)
-];
-var traceParcours= new google.maps.Polyline({
-	path: parcours,
-	strokeColor: "#34C924",
-	strokeOpacity: 1.0,
-	strokeWeight: 2
-});
+class Parcours extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			places: {},
+			location: [],
+			user: {
+			  "lat": 50.8503,
+			  "lng": 4.3517
+			}
+		  }
+	}
 
-traceParcoursBus.setMap(carte);
+	CcomponentDidMount() {
+		fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${this.state.user.lat},${this.state.user.lng}&destination=${this.state.user.lat},${this.state.user.lng}&waypoints=optimize:true|${type}&mode=walking&key=AIzaSyCPzxx1Hx18ZT4q2ONjkyFWYRVhlmNrN-I`)
+			, {
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				}
+			})
+		  .then(response => response.json())
+			.then(data => {
+				this.setState({
+					location: data.results
+				});
+			});
+	}
+
+	render() {
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
+
+export default Parcours;
